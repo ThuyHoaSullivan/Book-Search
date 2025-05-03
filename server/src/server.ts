c15ce204acb122e2183d9558337dc52ff016cce3
Hoa <sullivan.thuyhoa@gmail.com>
2025-05-03 00:29:24 -0500

    updating

--- server/src/server.ts
+++ server/src/server.ts
@@ -53,9 +53,9 @@ async function startServer() {
     );
 
     if (process.env.NODE_ENV === 'production') {
-      app.use(express.static(path.join(__dirname, '../client/build')));
+      app.use(express.static(path.join(__dirname, '../../client/build')));
       app.get('*', (_, res) => {
-        res.sendFile(path.join(__dirname, '../client/build/index.html'));
+        res.sendFile(path.join(__dirname, '../../client/build/index.html'));
       });
     }
 
